80dcb0e7aaaa9ee5b055ab0b753740d79c9a8356
zhearing <zyzhong@mail.ustc.edu.cn>
2018-05-20 16:49:05 +0800

    initial commit

---
+++ common/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+from .load_annotation import *
+from .image_draw import *
+from .write import *
+
+__all__ = [
+    'load_annotation',
+    'image_draw_line_list',
+]
