d25f6009b43e94715f15f2248abc24f1f2bddf93
zhearing <zyzhong@mail.ustc.edu.cn>
2018-05-21 11:18:38 +0800

    initial commit

--- Future_Driving_2017/task_1_Lane_Marking_Detection/data/transfer.py
+++ Future_Driving_2017/task_1_Lane_Marking_Detection/data/transfer.py
@@ -1,12 +1,30 @@
 #!/usr/bin/env python
 # coding=utf-8
+'''
+'raw_file':
+'h_samples':
+'lanes':
+'originH':
+'originW':
+'color':
+'type':
+    self.parser.add_argument('--loadH',       type = int, default = 280, help = 'scale to H' )
+    self.parser.add_argument('--loadW',       type = int, default = 350, help = 'scale to W' )
+    self.parser.add_argument('--fineH',       type = int, default = 256, help = 'crop to H' )
+    self.parser.add_argument('--fineW',       type = int, default = 320, help = 'crop to W' )
+    self.parser.add_argument('--feaH',        type = int, default = 32,  help = 'feature for H')
+    self.parser.add_argument('--feaW',        type = int, default = 40,  help = 'feature for W')
+    self.parser.add_argument('--pos_thres',   type = int, default = 3,   help = 'positive threshold')
+    self.parser.add_argument('--neg_thres',   type = int, default = 4,   help = 'negative threshold')
+    self.parser.add_argument('--negpos_ratio',type = int, default = 10,  help = 'neg: pos ratio')
+    self.parser.add_argument('--slicing',     type = int, default = 64,  help = '# of slicing parallel lines')
+'''
 import numpy as np
 from PIL import Image
 import random
 from collections import OrderedDict
 import torchvision.transforms as transforms
 import torch
-import itertools
 
 class Transformer():
     def __init__(self, opt):
@@ -24,24 +42,11 @@ class Transformer():
                             transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]
         self.totensor = transforms.Compose(transforms_list)
 
-    def isout(self, p):
-        x = p[0]
-        y = p[1]
-        if 0 <= x and x < self.opt.fineW + 1 and 0 <= y and y < self.opt.fineH + 1:
-            return False
-        return True
-
-    def label_feature(self, lane_array, attr_array):
+    def label_feature(self, lane_array):
         eps = 1e-5
         # try to find positive samples
         record     = np.ones((self.opt.feaH, self.opt.feaW)) * (-1.0)
         record_min = np.ones((self.opt.feaH, self.opt.feaW)) * float('inf')
-
-        color      = torch.zeros(self.opt.feaH, self.opt.feaW)
-        color_mask = torch.zeros(self.opt.feaH, self.opt.feaW)
-        type       = torch.zeros(self.opt.feaH, self.opt.feaW)
-        type_mask  = torch.zeros(self.opt.feaH, self.opt.feaW)
-        
         slice_step_y = int(self.opt.fineH / self.opt.slicing)
         y_slices = list(range(0, self.opt.fineH, slice_step_y))       
         assert len(y_slices) == self.opt.slicing, 'Warning: slicing number wrong!'
@@ -53,7 +58,7 @@ class Transformer():
         for lane_id, lane in enumerate(lane_array):
             lanexs, laneys = lane[0], lane[1]
             if len(laneys) == 0:
-                print(lane[0], lane[1])
+                print((lane[0], lane[1]))
             yid = 0
             ylength = len(laneys)
             ldict = OrderedDict()
@@ -76,33 +81,7 @@ class Transformer():
                         if dis < record_min[fea_y][fea_x]: # and dis < self.opt.pos_thres:
                             record_min[fea_y][fea_x] = dis
                             record[fea_y][fea_x]     = lane_id
-                            color[fea_y][fea_x]      = attr_array[lane_id][0]
-                            type[fea_y][fea_x]       = attr_array[lane_id][1]
 
-                            # check legal ''' tmp = color[fea_y][fea_x] assert tmp == 0 or tmp == 1, 'color = %d, not right' % (tmp) tmp = type[fea_y][fea_x] assert tmp == 0 or tmp == 1, 'type = %d, not right' % (tmp) '''
-                            color_mask[fea_y][fea_x]  = 1.0
-                            co = color[fea_y][fea_x]
-                            if not (co == 0 or co == 1):
-                                color_mask[fea_y][fea_x] = 0.0
-
-                            type_mask[fea_y][fea_x]  = 1.0
-                            ty = type[fea_y][fea_x]
-                            if not (ty == 0 or ty == 1):
-                                type_mask[fea_y][fea_x] = 0.0
-
-            # append up 3 points
-            keys = list(ldict.keys())
-            keys.reverse()
-            if len(keys) >= 2:
-                for i in range(3):
-                    k1 = keys[-1]
-                    k2 = keys[-2]
-                    nv = 2.0 * k1 - k2
-                    if nv > 0:
-                        ldict[nv] = 2.0 * ldict[k1] - ldict[k2]
-                        keys.append(nv)
-                    else:
-                        break
             lane_dict.append(ldict)
 
         # filling class matrix
@@ -133,11 +112,6 @@ class Transformer():
 
         res_cls      = torch.from_numpy(cls).unsqueeze(0).float() #.unsqueeze(0).float()
         res_cls_mask = torch.from_numpy(cls_mask).unsqueeze(0).float() #.unsqueeze(0).float()
-        res_color    = color.unsqueeze(0).float()
-        res_type     = type.unsqueeze(0).float()
-        #res_attr_mask= attr_mask.unsqueeze(0).float()
-        res_color_mask = color_mask.unsqueeze(0).float()
-        res_type_mask  = type_mask.unsqueeze(0).float()
 
         # filling up, down
         res_up        = torch.zeros((self.opt.slicing + 1, self.opt.feaH, self.opt.feaW))
@@ -160,14 +134,12 @@ class Transformer():
                 # for up
                 y_start = stdy
                 y_id    = 0
-                scale   = 1.0
                 while True:
                     if not y_start in list(ld.keys()):
                         break
                     dx = ld[y_start] - stdx
                     res_up[y_id + 1][h][w]      = dx
-                    res_up_mask[y_id + 1][h][w] = 1 * scale
-                    scale   += 0.1
+                    res_up_mask[y_id + 1][h][w] = 1
                     y_id    += 1
                     y_start -= slice_step_y
                 # number to regress
@@ -176,8 +148,6 @@ class Transformer():
                 
                 y_start = stdy + slice_step_y
                 y_id    = 0
-                out_time = 0
-                out_thres = 8
                 while True:
                     if not y_start in list(ld.keys()):
                         break
@@ -185,28 +155,11 @@ class Transformer():
                     res_down[y_id + 1][h][w]      = dx
                     res_down_mask[y_id + 1][h][w] = 1
                     y_id    += 1
-                    # down add out_thres more out most
-                    if self.isout([ld[y_start], y_start]):
-                        out_time += 1
-                    if out_time == out_thres:
-                        break
                     y_start += slice_step_y
-
-                # add more
-                for i in range(out_thres - out_time):
-                    if y_id - 1 > 0 and y_id + 1  < self.opt.slicing + 1:
-                        res_down[y_id + 1][h][w]      = 2.0 * res_down[y_id][h][w] - res_down[y_id - 1][h][w]
-                        res_down_mask[y_id + 1][h][w] = 1
-                        y_id   += 1
-                    else:
-                        break
-
                 # number to regress
                 res_down[0][h][w]      = y_id
                 res_down_mask[0][h][w] = 1
-
-        d = {'cls': res_cls, 'cls_mask': res_cls_mask, 'up': res_up, 'up_mask': res_up_mask, 'down': res_down, 'down_mask': res_down_mask,
-             'color': res_color, 'color_mask': res_color_mask, 'type': res_type, 'type_mask': res_type_mask}
+        d = {'cls': res_cls, 'cls_mask': res_cls_mask, 'up': res_up, 'up_mask': res_up_mask, 'down': res_down, 'down_mask': res_down_mask}
         #return res_cls, res_cls_mask, res_up, res_up_mask, res_down, res_down_mask
         return d
 
@@ -236,16 +189,13 @@ class Transformer():
 
     # parse label into array, fitting, and calculate the last < 0
     def parse_label(self, label):
-        y_mul = 1.0 / label['originH'] * self.opt.loadH
-        x_mul = 1.0 / label['originW'] * self.opt.loadW
+        #y_mul = 1.0 / label['originH'] * self.opt.loadH
+        #x_mul = 1.0 / label['originW'] * self.opt.loadW
+        y_mul = 1.0 / 720 * self.opt.loadH
+        x_mul = 1.0 / 1280 * self.opt.loadW
         lane_array = []
-        attr_array = []
         std_ys = np.array(label['h_samples']) * y_mul
-
-        assert (len(label['lanes']) == len(label['color']))
-        assert (len(label['lanes']) == len(label['type']))
-
-        for lane, co, ty in zip(label['lanes'], label['color'], label['type']):
+        for lane in label['lanes']:
             #print('lane x len = ', len(lane))
             #if self.opt.debug == 1:
             #    print np.array(lane)
@@ -257,36 +207,30 @@ class Transformer():
             xs, ys, need_delete = self.last_deal(xs, std_ys)
             if not need_delete:
                 lane_array.append([xs, ys])
-                attr_array.append([co, ty])
-        return lane_array, attr_array
+        return lane_array
 
 
     def gao_image_path(self, image_path):
         image = Image.open(image_path).convert('RGB')
         width, height = image.size
-        self.original_width = width
-        self.original_height = height
-        if width != self.opt.fineW or height != self.opt.fineH:
-            #image = image.resize((self.opt.loadW, self.opt.loadH), Image.BILINEAR)
-            image = image.resize((self.opt.fineW, self.opt.fineH), Image.BILINEAR)
+        if width != self.opt.loadW or height != self.opt.loadH:
+            image = image.resize((self.opt.loadW, self.opt.loadH), Image.BILINEAR)
         image = self.totensor(image)
         return image
 
 
     def gao(self, image, label, phase):
-        lane_array, attr_array = self.parse_label(label)
+        lane_array = self.parse_label(label)
         if phase == 'train':
             width, height = image.size
             if width != self.opt.loadW or height != self.opt.loadH:
                 image = image.resize((self.opt.loadW, self.opt.loadH), Image.BILINEAR)
-
             # ------------------------------------------------------------------------
             # crop !
             # parallel gao image and label
             dx = random.randint(0, self.opt.dw)
             dy = random.randint(0, self.opt.dh)
             image = image.crop((dx, dy, dx + self.opt.fineW, dy + self.opt.fineH))
-
             for i in range(len(lane_array)):
                 lane_array[i][0] = lane_array[i][0] - dx
                 lane_array[i][1] = lane_array[i][1] - dy
@@ -301,19 +245,8 @@ class Transformer():
                     lane_array[i][0] = self.opt.fineW - 1 - lane_array[i][0]
         
         image = self.totensor(image)
-        # ------------------------------------------------------------------------
-        # cutout!
-        if self.opt.cutout > 0:
-            C, height, width = image.shape
-            assert(C == 3 and height == self.opt.fineH and width == self.opt.fineW)
-            cx = random.randint(0, width - 1)
-            cy = random.randint(int(2.0 / 3 * height), height - 1)
-            edge = self.opt.cutout / 2
-            lp = (cx - edge if cx > edge else 0, cy - edge if cy > edge else 0)
-            rp = (cx + edge if cx + edge <= width else width, cy + edge if cy + edge <= height else height)
-            # cutout
-            image[:, lp[1]: rp[1], lp[0]: rp[0]] = 0
-
-        label = self.label_feature(lane_array, attr_array)
+        label = self.label_feature(lane_array)
         #print('image.shape = ', image.shape)
         return image, label
+
+
